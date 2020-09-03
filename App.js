031ca99d0fe57421f1d3d24cbe4f235fb36c7c58
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-09-03 10:30:18 +0900

    details page

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Slide from './components/pictureslider'
+import Slide from './components/pictureslider';
+import Details from './components/details'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Slide></Slide>
+      <Details></Details>
       <StatusBar style="auto" />
     </View>
   );
