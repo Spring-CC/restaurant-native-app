44a8f6df7b42092c4a2836fa3f7633c7cd71e459
Eri Shimada <shimaeri515@gmail.com>
2020-09-08 09:17:19 +0900

    adding paython

---
+++ recommender/machine.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+# collaborative filtering 
+## user-based
+
+# get data from mongodb
+### swiped data -- merge -- userdata ----> csv file
+swipedyes_df = pd.read_csv('')
+user_df = pd.read_csv('')
+swipedyes = usr_df ##
+## merge these two 
+filtering_df = pd.read_csv('')
+
+
