60ab13ce5afbf5e193ff37d38509c67646626b36
Eri Shimada <shimaeri515@gmail.com>
2020-09-09 10:30:41 +0900

    working on sparse matrix

--- recommender/machine.py
+++ recommender/machine.py
@@ -1,41 +1,51 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from ast import literal_eval
 ####
-import re
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.neighbors import NearestNeighbors
-from scipy.sparse import csr_matrix
+# import re
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# from sklearn.neighbors import NearestNeighbors
+# from scipy.sparse import csr_matrix
 
-print("!!!!!!!")
+
 # collaborative filtering 
 ## user-based
-
 
 #  swiped data -- merge -- userdata ----> csv file
 ###### connect to data
 swipeddata_df = pd.read_csv('data/dummydata.csv')
 # show the dataframe first 5
-print(swipeddata_df.head())
-
-
-## check the top list
-# fig, ax = plt.subplots  #(figsize=[5,10])
-# sns.countplot(data=swipeddata_df[swiped_df['swiped_right'].isin(
-#     categories['swiped_right'].value_counts().head(25).index)],
-#                               y='swiped_right', ax=ax)
-# plt.show()
-
+#print(swipeddata_df.head())
 
 ## clean up the data
-piv = swipeddata_df.pivot_table(index = ["userid"],columns=["name"],values="swipeddata")
-piv.head() 
-piv.shape
+# piv = swipeddata_df.pivot_table(index = ["userid"],columns=["name"],values="swipeddata")
+# piv.head() 
+# piv.shape
+
+## sparse matrix
+# rest_list = swipeddata_df.swiped_right
+# df = pd.DataFrame(rest_list,
+#                   index=[1,2,3,4])
+# print(df)
+
+# print(rest_list.swiped_right[0])
+swipeddata_df = pd.read_csv('data/dummydata.csv', usecols=[0,2], index_col=0 )
+# new_df = swipeddata_df.explode('swiped_right')
+# show the dataframe first 5
+swipeddata_df.swiped_right = swipeddata_df.swiped_right.apply(literal_eval)
+new_df = swipeddata_df.explode("swiped_right")
+print(new_df)
+
+# user_pivot =  swipeddata_df.pivot(index='name', columns='userid', values='swiped_right').fillna(0)
+# print(user_pivot)
+
+
 #Min-Max Scaling
 # change NaN to 0
-piv_norm = piv.apply(lambda x:(x-np.mean(x))/(np.max(x)-np.min(x)),axis=1)
-piv_norm.fillna(0,inplace=True) 
+# piv_norm = piv.apply(lambda x:(x-np.mean(x))/(np.max(x)-np.min(x)),axis=1)
+# piv_norm.fillna(0,inplace=True) 
 
 # create cross tabulation userid × restaurantid
 swiped_list = swipeddata_df.sort_values('userid').userid.unique()
