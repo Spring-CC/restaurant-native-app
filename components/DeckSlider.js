2debaa2e48a63983cf54696e73bed1e0f829fb62
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-09-11 16:21:33 +0900

    added deck slider, there is a problem getting the card info to the details page

---
+++ components/DeckSlider.js
@@ -0,0 +1,141 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import {
+    StyleSheet,
+    ScrollView,
+    Image,
+} from "react-native";
+import {
+    Container,
+    View,
+    DeckSwiper,
+    Card,
+    CardItem,
+    Left,
+    Body,
+    Footer,
+    FooterTab,
+    Button,
+    Icon,
+    Text,
+} from 'native-base';
+import { useSelector, useDispatch } from "react-redux";
+import { setRestaurantsList } from "../actions";
+import Nav from "./Nav";
+
+export default function Home({ navigation }) {
+
+    const price = useSelector((state) => state.priceReducer);
+    const restaurantList = useSelector((state) => state.restaurantsListReducer);
+
+    const dispatch = useDispatch();
+
+    async function getRestaurants() {
+        try {
+            const results = await axios.get("https://restaurantserverspring.herokuapp.com/restAtlas");
+            const restaurants = results.data;
+            const filtBudget = restaurants.filter(
+                (res) => res.budget >= price.min && res.budget <= price.max
+            );
+            dispatch(setRestaurantsList(filtBudget));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getRestaurants();
+    }, []);
+
+    function onSwipeRight() {
+        // navigation.navigate("Details");
+    }
+
+    return (
+        <Container>
+            <Nav />
+            <View style={styles.container}>
+                <DeckSwiper
+                    dataSource={restaurantList}
+                    onSwipeRight={() => onSwipeRight()}
+                    onSwipeLeft={() => console.log("No")}
+                    renderItem={item =>
+                        <Card style={{ elevation: 3 }} >
+                            <CardItem>
+                                <Left>
+                                    <Body>
+                                        <Text style={styles.text}>Restaurant</Text>
+                                        <Text note style={styles.text}>Swipe left for 'No' and right for 'Yes'</Text>
+                                    </Body>
+                                </Left>
+                            </CardItem>
+                            <CardItem cardBody>
+                                <Image
+                                    style={{ height: 300, flex: 1 }}
+                                    source={{
+                                        uri: item.image_url["shop_image1"],
+                                    }} />
+                            </CardItem>
+                            <CardItem>
+                                <ScrollView>
+                                    <Body >
+                                        <Text style={styles.text}>Name: </Text>
+                                        <Text style={styles.text}>{item.name}</Text>
+                                        <Text style={styles.text}>Type of Restaurant: </Text>
+                                        <Text style={styles.text}>{item.category}</Text>
+                                        <Text style={styles.text}>Station:</Text>
+                                        <Text style={styles.text}>{item.access["station"]}</Text>
+                                        <Text style={styles.text}>Open Hours: </Text>
+                                        <Text style={styles.text}>{item.opentime}</Text>
+                                    </Body>
+                                </ScrollView>
+                            </CardItem>
+                        </Card>
+                    }
+                    renderEmpty={() =>
+                        <Card style={{ elevation: 3 }}>
+                            <CardItem>
+                                <Text>No restaurants match your selected preference.</Text>
+                            </CardItem>
+                        </Card>
+                    }
+                />
+            </View>
+            <Footer>
+                <FooterTab>
+                    <Button vertical onPress={() => navigation.navigate("Home")}>
+                        <Icon name="home" />
+                        <Text>Home</Text>
+                    </Button>
+                    <Button active vertical onPress={() => navigation.navigate("Search")}>
+                        <Icon name="eye" />
+                        <Text>Search</Text>
+                    </Button>
+                    <Button vertical onPress={() => navigation.navigate("Preferences")}>
+                        <Icon active name="pizza" />
+                        <Text>Preference</Text>
+                    </Button>
+                    <Button vertical onPress={() => navigation.navigate("Favorites")}>
+                        <Icon name="heart" />
+                        <Text>Favorites</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F9C74F",
+        padding: 5,
+    },
+    text: {
+        fontFamily: "MPLUS1p-Bold",
+    },
+    text_sub: {
+        fontFamily: "MPLUS1p-Bold",
+        textDecorationLine: "underline",
+    },
+});
