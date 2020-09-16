7054e2654fa50988686313a292c9e70f6f97acaf
Dylan Cooper <dylan.cooper55@gmail.com>
2020-09-16 15:08:39 +0900

    added color to titles on card

--- components/Home.js
+++ components/Home.js
@@ -42,14 +42,14 @@ export default function Home({ navigation }) {
     console.log("In recommended");
     const results = await axios.get(
       `https://restaurantserverspring.herokuapp.com/dummyfavorites/${user}`
-      
+
     );
     const data = results.data;
-    
+
     console.log(data);
     dispatch(setRestaurantsList(data));
   }
-  
+
   useEffect(() => {
     if (userId === "") {
       getRestaurants();
@@ -138,13 +138,13 @@ export default function Home({ navigation }) {
               <CardItem>
                 <ScrollView>
                   <Body>
-                    <Text style={styles.text}>Name:</Text>
+                    <Text style={styles.text_sub}>Name:</Text>
                     <Text style={styles.text}>{item.name}</Text>
-                    <Text style={styles.text}>Type of Restaurant:</Text>
+                    <Text style={styles.text_sub}>Type of Restaurant:</Text>
                     <Text style={styles.text}>{item.category}</Text>
-                    <Text style={styles.text}>Station:</Text>
+                    <Text style={styles.text_sub}>Station:</Text>
                     <Text style={styles.text}>{item.access["station"]}</Text>
-                    <Text style={styles.text}>Open Hours:</Text>
+                    <Text style={styles.text_sub}>Open Hours:</Text>
                     <Text style={styles.text}>{item.opentime}</Text>
                   </Body>
                 </ScrollView>
@@ -188,6 +188,6 @@ const styles = StyleSheet.create({
   },
   text_sub: {
     fontFamily: "MPLUS1p-Bold",
-    textDecorationLine: "underline",
+    color: '#F3722C',
   },
 });
