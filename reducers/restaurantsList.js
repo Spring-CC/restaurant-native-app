cf9cea04f64f4722d0a7842b2be20647f886317e
Eri Shimada <shimaeri515@gmail.com>
2020-09-18 14:03:58 +0900

    adding refucer

--- reducers/restaurantsList.js
+++ reducers/restaurantsList.js
@@ -18,6 +18,18 @@ const restaurantsListReducer = (state = initialState, action) => {
       }
       return restaurants;
     }
+    case "SORT_RESTAURANTS_END": {
+        let restaurants = [...state]; // coying all restaurant data
+        for(let i = 0; i< action.payload.length; i++){ // recommended restaurabt
+            for(let j=0; j < restaurants.length; j++){ // current restaurant state
+              if(action.payload[i].id === restaurants[j].id){ 
+                  restaurants.splice(j,1)  // remove duplicated
+                  restaurants.push(action.paylod[i]) // move it to the back
+              }
+            }
+        }
+        return restaurants;
+      }
     default:
       return state;
   }
