7857588c63788935742e5d6267d2fa1d93f0a97a
Alberto Medellin <albertomedellin85@gmail.com>
2020-09-19 15:51:58 -0700

    Fix bug in priceRange reducer

--- components/Home.js
+++ components/Home.js
@@ -87,6 +87,10 @@ export default function Home({ navigation }) {
     }
   }, []);
 
+  useEffect(()=>{
+    console.log(restaurantList[0].name);
+  })
+
   async function liked(user, restaurant) {
     try {
       const likedResId = restaurant.id;
