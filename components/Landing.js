7d76bfc397373e952dae64748265688814c64793
Dylan Cooper <dylan.cooper55@gmail.com>
2020-09-23 13:05:19 +0900

    hard code name for demo day

--- components/Landing.js
+++ components/Landing.js
@@ -102,7 +102,7 @@ export default function Landing({ navigation }) {
       {isLoggedIn ? (
         <View>
           <View>
-            <Text style={styles.welcometext}>Welcome, {name}!</Text>
+            <Text style={styles.welcometext}>Welcome, Sam!</Text>
           </View>
           <View style={styles.buttonContainer}>
             <TouchableOpacity
